34dacf636eab9a9ebc91433567619ee5d80a65fb
Stefan Schupp <stefan.schupp@cs.rwth-aachen.de>
2022-02-22 17:42:49 +0100

    add test for grid-coverage heuristic

--- src/training/TrainingHeuristics.h
+++ src/training/TrainingHeuristics.h
@@ -70,8 +70,8 @@ struct Grid {
     LocPtr loc  = *std::next( automaton.getLocations().begin(), locIndex );
 
     // create sample
-    Point sample{ hypro::vector_t<Number>::Zero( settings.samplingArea.size() ) };
-    for ( auto i = 0; i < settings.samplingArea.size(); ++i ) {
+    Point sample{ hypro::vector_t<Number>::Zero( settings.samplingArea.dimension() ) };
+    for ( auto i = 0; i < settings.samplingArea.dimension(); ++i ) {
       const auto& interval = settings.samplingArea.at( i );
       sample[i]            = ( interval.upper() - interval.lower() ) / ( resolution[i] + 1 ) * ( currentCell[i] + 1 );
     }
@@ -89,29 +89,49 @@ struct Grid {
 };
 
 struct GridCover {
+  GridCover( const std::vector<std::size_t>& resolution, const TrainingSettings& settings )
+      : resolution( resolution ),
+        currentCell( std::vector<std::size_t>( 0, resolution.size() ) ),
+        gen( resolution, settings.samplingArea.dimension() ) {
+    if ( resolution.size() != settings.samplingArea.dimension() ) {
+      throw std::logic_error( "Please specify a number of splittings for every dimension." );
+    }
+  }
+
   locationConditionMap operator()( const hypro::HybridAutomaton<Number>& automaton, const TrainingSettings& settings ) {
-    locationConditionMap                       res;
-    std::mt19937                               generator;
-    std::uniform_int_distribution<std::size_t> LocPtr_dist{ 0, automaton.getLocations().size() - 1 };
-    std::size_t                                chosenLocPtr = LocPtr_dist( generator );
-    LocPtr                                     loc = *std::next( automaton.getLocations().begin(), chosenLocPtr );
+    locationConditionMap res;
+    if ( gen.end() ) {
+      gen.reset();
+      locIndex++;
+    }
+    currentCell = gen();
+    LocPtr loc  = *std::next( automaton.getLocations().begin(), locIndex );
 
     // create sample
-    Point sample{ hypro::vector_t<Number>::Zero( settings.samplingArea.size() ) };
-    for ( auto i = 0; i < settings.samplingArea.size(); ++i ) {
-      std::uniform_real_distribution<Number> sample_dist{ settings.samplingArea.interval( i ).lower(),
-                                                          settings.samplingArea.interval( i ).upper() };
-      sample[i] = sample_dist( generator );
+    std::vector<carl::Interval<Number>> intervals;
+    for ( auto i = 0; i < settings.samplingArea.dimension(); ++i ) {
+      if ( resolution[i] == 1 ) {
+        intervals.emplace_back( settings.samplingArea.at( i ) );
+      } else {
+        const auto& interval   = settings.samplingArea.at( i );
+        Number      width      = ( interval.upper() - interval.lower() ) / resolution[i];
+        Number      lowerBound = interval.lower() + currentCell[i] * width;
+        Number      upperBound = interval.lower() + ( currentCell[i] + 1 ) * width;
+        intervals.emplace_back( lowerBound, upperBound );
+      }
     }
+    assert( intervals.size() == settings.samplingArea.dimension() );
     // widen sample, if required
-    res[loc] = hypro::Condition<Number>{ widenSample( sample, settings.initialSetWidth, settings.wideningDimensions ) };
+    res[loc] = hypro::conditionFromIntervals( intervals );
 
     return res;
   }
 
  private:
-  std::vector<std::size_t> maximalIndices;
+  std::vector<std::size_t> resolution;
   std::vector<std::size_t> currentCell;
+  hypro::Combinator        gen;
+  std::size_t              locIndex = 0;
 };
 
 }  // namespace simplexArchitectures
--- test/TrainerTest.cpp
+++ test/TrainerTest.cpp
@@ -71,6 +71,69 @@ TEST_CASE( "Grid-sample generation", "[training]" ) {
   REQUIRE( !std::begin( s4 )->second.contains( hypro::Point<Number>{ 0.25, 0.25, 1, 0, 0 } ) );
 }
 
+TEST_CASE( "Grid-cover-sample generation", "[training]" ) {
+  using Interval = carl::Interval<Number>;
+  using IVector  = std::vector<Interval>;
+  using Box      = hypro::Box<Number>;
+  auto [automaton, _] =
+      hypro::parseFlowstarFile<Number>( simplexArchitectures::getModelsPath() + "21_simplex_watertanks.model" );
+  std::vector<std::size_t>               resolution{ 2, 2, 1, 1, 1 };
+  simplexArchitectures::TrainingSettings trainingSettings{
+      5,
+      simplexArchitectures::INITIAL_STATE_HEURISTICS::GRID_COVER,
+      { 0, 1 },
+      Box{ IVector{ Interval{ 0, 1 }, Interval{ 0, 1 }, Interval{ 0 }, Interval{ 0 }, Interval{ 0 } } } };
+  simplexArchitectures::GridCover g{ resolution, trainingSettings };
+  auto                            s1 = g( automaton, trainingSettings );
+  CAPTURE( std::begin( s1 )->second );
+  std::cout << std::begin( s1 )->second << std::endl;
+  REQUIRE( s1.size() == 1 );
+  REQUIRE( std::begin( s1 )->second.dimension() == trainingSettings.samplingArea.dimension() );
+  REQUIRE( std::begin( s1 )->second.contains( hypro::Point<Number>{ 0.33, 0.33, 0, 0, 0 } ) );
+  REQUIRE( !std::begin( s1 )->second.contains( hypro::Point<Number>{ 0.66, 0.66, 0, 0, 0 } ) );
+  REQUIRE( !std::begin( s1 )->second.contains( hypro::Point<Number>{ 0.25, 0.25, 1, 0, 0 } ) );
+  auto b1 = Box( std::begin( s1 )->second.getMatrix(), std::begin( s1 )->second.getVector() );
+  REQUIRE( b1.intervals()[0] == Interval{ 0.0, 0.5 } );
+  REQUIRE( b1.intervals()[1] == Interval{ 0.0, 0.5 } );
+  REQUIRE( b1.intervals()[2] == Interval{ 0.0 } );
+  REQUIRE( b1.intervals()[3] == Interval{ 0.0 } );
+  REQUIRE( b1.intervals()[4] == Interval{ 0.0 } );
+
+  auto s2 = g( automaton, trainingSettings );
+  REQUIRE( s2.size() == 1 );
+  REQUIRE( std::begin( s2 )->second.contains( hypro::Point<Number>{ 0.66, 0.33, 0, 0, 0 } ) );
+  REQUIRE( !std::begin( s2 )->second.contains( hypro::Point<Number>{ 0.66, 0.66, 0, 0, 0 } ) );
+  REQUIRE( !std::begin( s2 )->second.contains( hypro::Point<Number>{ 0.25, 0.25, 1, 0, 0 } ) );
+  auto b2 = Box( std::begin( s2 )->second.getMatrix(), std::begin( s2 )->second.getVector() );
+  REQUIRE( b2.intervals()[0] == Interval{ 0.5, 1.0 } );
+  REQUIRE( b2.intervals()[1] == Interval{ 0.0, 0.5 } );
+  REQUIRE( b2.intervals()[2] == Interval{ 0.0 } );
+  REQUIRE( b2.intervals()[3] == Interval{ 0.0 } );
+  REQUIRE( b2.intervals()[4] == Interval{ 0.0 } );
+  auto s3 = g( automaton, trainingSettings );
+  REQUIRE( s3.size() == 1 );
+  REQUIRE( std::begin( s3 )->second.contains( hypro::Point<Number>{ 0.33, 0.66, 0, 0, 0 } ) );
+  REQUIRE( !std::begin( s3 )->second.contains( hypro::Point<Number>{ 0.66, 0.66, 0, 0, 0 } ) );
+  REQUIRE( !std::begin( s3 )->second.contains( hypro::Point<Number>{ 0.25, 0.25, 1, 0, 0 } ) );
+  auto b3 = Box( std::begin( s3 )->second.getMatrix(), std::begin( s3 )->second.getVector() );
+  REQUIRE( b3.intervals()[0] == Interval{ 0.0, 0.5 } );
+  REQUIRE( b3.intervals()[1] == Interval{ 0.5, 1.0 } );
+  REQUIRE( b3.intervals()[2] == Interval{ 0.0 } );
+  REQUIRE( b3.intervals()[3] == Interval{ 0.0 } );
+  REQUIRE( b3.intervals()[4] == Interval{ 0.0 } );
+  auto s4 = g( automaton, trainingSettings );
+  REQUIRE( s4.size() == 1 );
+  REQUIRE( std::begin( s4 )->second.contains( hypro::Point<Number>{ 0.66, 0.66, 0, 0, 0 } ) );
+  REQUIRE( !std::begin( s4 )->second.contains( hypro::Point<Number>{ 0.33, 0.66, 0, 0, 0 } ) );
+  REQUIRE( !std::begin( s4 )->second.contains( hypro::Point<Number>{ 0.25, 0.25, 1, 0, 0 } ) );
+  auto b4 = Box( std::begin( s4 )->second.getMatrix(), std::begin( s4 )->second.getVector() );
+  REQUIRE( b4.intervals()[0] == Interval{ 0.5, 1.0 } );
+  REQUIRE( b4.intervals()[1] == Interval{ 0.5, 1.0 } );
+  REQUIRE( b4.intervals()[2] == Interval{ 0.0 } );
+  REQUIRE( b4.intervals()[3] == Interval{ 0.0 } );
+  REQUIRE( b4.intervals()[4] == Interval{ 0.0 } );
+}
+
 TEST_CASE( "Random Training with a single iteration" ) {
   using Interval = carl::Interval<Number>;
   using IVector  = std::vector<Interval>;
