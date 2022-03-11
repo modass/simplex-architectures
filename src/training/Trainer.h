f8ab48f293847737488b1e60e705497c94137506
Stefan Schupp <stefan.schupp@tuwien.ac.at>
2022-03-11 14:59:07 +0100

    formatting and documentation
    Signed-off-by: Stefan Schupp <stefan.schupp@tuwien.ac.at>

--- src/training/Trainer.h
+++ src/training/Trainer.h
@@ -8,19 +8,18 @@
 #include <hypro/datastructures/Hyperoctree.h>
 #include <hypro/datastructures/reachability/ReachTreev2.h>
 #include <hypro/datastructures/reachability/Settings.h>
+#include <spdlog/spdlog.h>
 
 #include <cereal/archives/binary.hpp>
 #include <cereal/archives/xml.hpp>
 #include <cereal/types/map.hpp>
-#include <fstream>
-#include <spdlog/spdlog.h>
 #include <filesystem>
+#include <fstream>
 
 #include "../types.h"
 #include "../utility/fileExists.h"
 #include "TrainingHeuristics.h"
 #include "TrainingSettings.h"
-#include "TrainingHeuristics.h"
 
 namespace simplexArchitectures {
 
@@ -41,27 +40,27 @@ class Trainer {
         mStorageSettings( storageSettings ) {
     if ( fileExists( mFilename ) ) {
       {
-        std::ifstream              fs{ mFilename, std::ios::binary };
-        //std::ifstream              fs{ mFilename };
+        std::ifstream fs{ mFilename, std::ios::binary };
+        // std::ifstream              fs{ mFilename };
         cereal::BinaryInputArchive iarchive( fs );
         // cereal::XMLInputArchive iarchive( fs );
         iarchive( mStorageSettings, mTrees );
       }
-      spdlog::info("Read {} octrees which store {} sets from file", mTrees.size(), this->size());
-      for(const auto& [name,tree] : mTrees) {
-        spdlog::debug("Have tree for location {} which stores {} sets", name, tree.size());
+      spdlog::info( "Read {} octrees which store {} sets from file", mTrees.size(), this->size() );
+      for ( const auto& [name, tree] : mTrees ) {
+        spdlog::debug( "Have tree for location {} which stores {} sets", name, tree.size() );
       }
     }
   }
   ~Trainer() {
     // write tree to file upon destruction
-    spdlog::info("Write {} octrees which store {} sets to file.", mTrees.size(), this->size());
+    spdlog::info( "Write {} octrees which store {} sets to file.", mTrees.size(), this->size() );
     // if the file exists, use it as a backup
-    if( fileExists(mFilename)) {
-      std::filesystem::rename(mFilename, mFilename +".bak");
+    if ( fileExists( mFilename ) ) {
+      std::filesystem::rename( mFilename, mFilename + ".bak" );
     }
-    std::ofstream               fs{ mFilename, std::ios::binary };
-    //std::ofstream               fs{ mFilename };
+    std::ofstream fs{ mFilename, std::ios::binary };
+    // std::ofstream               fs{ mFilename };
     {
       cereal::BinaryOutputArchive oarchive( fs );
       // cereal::XMLOutputArchive oarchive( fs );
@@ -87,11 +86,31 @@ class Trainer {
   const std::map<std::string, hypro::Hyperoctree<Number>>& getTrainingData() const { return mTrees; }
 
  private:
+  /// Calls the chosen generator and returns a new set of initial states (location + state set)
   locationConditionMap generateInitialStates() const;
-  void                 updateOctree( const std::vector<hypro::ReachTreeNode<Representation>>& roots );
-  void                 runIteration( const hypro::Settings& settings, InitialStatesGenerator& generator );
+  /**
+   * Integrates the computed sets of reachable states stored in the reachability tree into the training storage
+   * @param roots reachability tree
+   */
+  void updateOctree( const std::vector<hypro::ReachTreeNode<Representation>>& roots );
+  /**
+   * Runs one training iteration, i.e., one run with one initial set and tries to find a fixed point
+   * @param settings settings for the reachability analysis
+   * @param generator generator which implements a strategy for training and creates novel initial sets
+   */
+  void runIteration( const hypro::Settings& settings, InitialStatesGenerator& generator );
+  /**
+   * Getter for the number of stored safe sets
+   * @return Number of stored safe sets
+   */
   std::size_t size() const;
+  /**
+   * Getter for the initial state generator according to the heuristic specified in the training settings
+   * @return pointer to a generator
+   */
   InitialStatesGenerator* getInitialStatesGenerator() const;
+  /// Helper-function which determines how many initial states need to be generated to achieve full coverage of the
+  /// training area when using GRID_COVERAGE
   std::size_t computeRequiredIterationsForFullCoverage() const;
 
  protected:
