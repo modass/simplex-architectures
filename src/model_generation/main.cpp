1e812060ce7fce84596b716fd4b3b56024164791
Stefan Schupp <stefan.schupp@tuwien.ac.at>
2022-03-23 12:19:21 +0100

    include model generation in the project
    * add library target
    * add small application that generates models
    
    Signed-off-by: Stefan Schupp <stefan.schupp@tuwien.ac.at>

---
+++ src/model_generation/main.cpp
@@ -0,0 +1,38 @@
+/*
+ * Created by Stefan Schupp <stefan.schupp@tuwien.ac.at> on 23.03.22.
+ */
+#include <string>
+#include <CLI/CLI.hpp>
+#include <CLI/App.hpp>
+#include <spdlog/spdlog.h>
+#include "generateBicycle.h"
+
+enum MODEL { PURE_PURSUIT };
+
+int main( int argc, char** argv ) {
+  spdlog::set_level( spdlog::level::debug );
+
+  std::map<std::string, MODEL> map{ { "pursuit", MODEL::PURE_PURSUIT } };
+  std::string keys{"pursuit"};
+
+  CLI::App app{ "Model generation tool" };
+
+  std::string                                     storagefilename = "out";
+  std::size_t discretization = 8;
+  std::pair<double,double> delta_range{-60.0, 60.0};
+  MODEL model = MODEL::PURE_PURSUIT;
+  app.add_option( "-d,--discretization", discretization, "Discretization for hybridization" )->check( CLI::PositiveNumber );
+  app.add_option( "-m,--model", model,
+                  "What model should be generated. Possible values: " + keys )
+      ->transform( CLI::CheckedTransformer( map, CLI::ignore_case ) );
+  app.add_option( "-o,--outfile", storagefilename, "Path to output file" );
+
+  CLI11_PARSE( app, argc, argv );
+
+  switch ( model ) {
+    case MODEL::PURE_PURSUIT:{
+      modelGenerator::generateBicycleModelFile(storagefilename,delta_range,discretization);
+      break;
+    }
+  }
+}
