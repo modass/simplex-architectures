cef39b673e1d5cc4bf40266c9a6399b7bcf6ec66
Stefan Schupp <stefan.schupp@tuwien.ac.at>
2022-06-22 15:52:40 +0200

    Generalize Executor
    * add settings structure
    * settings are used to chose dimensions for control and to locate the cycle clock
    
    Signed-off-by: Stefan Schupp <stefan.schupp@tuwien.ac.at>

--- src/simulation/Executor.cpp
+++ src/simulation/Executor.cpp
@@ -6,23 +6,24 @@
 #include <spdlog/spdlog.h>
 
 Point simplexArchitectures::Executor::execute(const Point& ctrlInput) {
-    roots.clear();
+  roots.clear();
 
-    hypro::Point<Number> extendedState = hypro::Point<Number>(hypro::vector_t<Number>::Zero(5));
-    extendedState[0] = mLastState[0];
-    extendedState[1] = mLastState[1];
-    extendedState[2] = ctrlInput[0];
-    std::stringstream  ss;
-    ss << extendedState;
-    spdlog::debug("Run executor with initial state {} in location {}", ss.str(), mLastLocation->getName());
-    // create intervals representing the initial state
-    std::vector<carl::Interval<Number>> intervals;
-    for ( Eigen::Index i = 0; i < extendedState.dimension(); ++i ) {
-        intervals.emplace_back( carl::Interval<Number>( extendedState.at( i ) ) );
-    }
-    auto initialBox = hypro::Condition<Number>{ intervals };
-    typename hypro::HybridAutomaton<Number>::locationConditionMap initialStates;
-    initialStates[mLastLocation] = initialBox;
+  hypro::Point<Number> extendedState = mLastState;
+  // if any controll has been provided, set it here
+  for ( Eigen::Index d : mExecutionSettings.control_input_dimensions ) {
+    extendedState[d] = ctrlInput[d];
+  }
+  std::stringstream ss;
+  ss << extendedState;
+  spdlog::debug( "Run executor with initial state {} in location {}", ss.str(), mLastLocation->getName() );
+  // create intervals representing the initial state
+  std::vector<carl::Interval<Number>> intervals;
+  for ( Eigen::Index i = 0; i < extendedState.dimension(); ++i ) {
+    intervals.emplace_back( carl::Interval<Number>( extendedState.at( i ) ) );
+  }
+  auto                                                          initialBox = hypro::Condition<Number>{ intervals };
+  typename hypro::HybridAutomaton<Number>::locationConditionMap initialStates;
+  initialStates[mLastLocation] = initialBox;
     mAutomaton.setInitialStates( initialStates );
     auto sampleRoots = hypro::makeRoots<Representation>( mAutomaton );
     // add roots for this sample to global reachtree
@@ -50,41 +51,42 @@ Point simplexArchitectures::Executor::execute(const Point& ctrlInput) {
     // Pick an artificial observation.
     std::map<LocPtr, Box> samplesBoxes;
     // create constraints which fix the time to the last tick
-    Matrix constraints = Matrix::Zero( 2, 5 );
+    Matrix constraints = Matrix::Zero( 2, mAutomaton.dimension() );
     Vector constants   = Vector::Zero( 2 );
     // tick
-    constraints( 0, 4 ) = 1;
-    constraints( 1, 4 ) = -1;
-    constants( 0 ) = 0;
-    constants( 1 ) = -0;
+    constraints( 0, mExecutionSettings.clock_dimension ) = 1;
+    constraints( 1, mExecutionSettings.clock_dimension ) = -1;
+    constants( 0 )                                       = 0;
+    constants( 1 )                                       = -0;
     // collect all leaf nodes that agree with the cycle time
     for ( auto& r : roots ) {
-        for ( auto& n : hypro::preorder( r ) ) {
-            if ( n.isLeaf() ) {
-                // I don't think we really need this check. We only consider initial sets of nodes that where reached by resetting the cLocPtrk to zero.
-                auto [containment, result] = n.getInitialSet().satisfiesHalfspaces( constraints, constants );
-                if ( containment != hypro::CONTAINMENT::NO ) {
-//                    std::cout << "[Simulator] New sample: " << result << std::endl;
-                    if ( samplesBoxes.find( n.getLocation() ) != samplesBoxes.end() ) {
+      for ( auto& n : hypro::preorder( r ) ) {
+        if ( n.isLeaf() ) {
+          // I don't think we really need this check. We only consider initial sets of nodes that where reached by
+          // resetting the cLocPtrk to zero.
+          auto [containment, result] = n.getInitialSet().satisfiesHalfspaces( constraints, constants );
+          if ( containment != hypro::CONTAINMENT::NO ) {
+            //                    std::cout << "[Simulator] New sample: " << result << std::endl;
+            if ( samplesBoxes.find( n.getLocation() ) != samplesBoxes.end() ) {
                         samplesBoxes[n.getLocation()] = samplesBoxes[n.getLocation()].unite( result );
                     } else {
-                        samplesBoxes[n.getLocation()] = result;
-                    }
-                }
+              samplesBoxes[n.getLocation()] = result;
             }
+          }
         }
+      }
     }
     // create an artificial observation
     std::uniform_int_distribution<std::size_t> LocPtr_dist{ 0, samplesBoxes.size() - 1 };
-    std::size_t chosenLocPtr = LocPtr_dist( mGenerator );
-    LocPtr location = std::next( samplesBoxes.begin(), chosenLocPtr )->first;
-    Point observation = samplesBoxes.at( location ).vertices().front().projectOn( { 0, 1 } );
+    std::size_t                                chosenLocPtr = LocPtr_dist( mGenerator );
+    LocPtr                                     location     = std::next( samplesBoxes.begin(), chosenLocPtr )->first;
+    Point observation = samplesBoxes.at( location ).vertices().front();  //.projectOn( { 0, 1 } );
 
     mLastLocation = location;
-    mLastState = observation;
-    ss.str(std::string());
+    mLastState    = observation;
+    ss.str( std::string() );
     ss << observation;
-    spdlog::debug("Executor has new state: {} in location {}", ss.str(), mLastLocation->getName());
+    spdlog::debug( "Executor has new state: {} in location {}", ss.str(), mLastLocation->getName() );
 
     return observation;
 }
--- src/simulation/Executor.h
+++ src/simulation/Executor.h
@@ -16,18 +16,27 @@
 
 namespace simplexArchitectures {
 
-    using Matrix = hypro::matrix_t<Number>;
-    using Vector = hypro::vector_t<Number>;
+/// Holds case-study specific parameters relevant for execution of the system
+struct ExecutionSettings {
+  Eigen::Index              clock_dimension;           ///< the state space dimension where the cycle-clock is stored
+  std::vector<Eigen::Index> control_input_dimensions;  ///< the state space dimensions which holds the control-input
+};
 
-    struct Executor {
-        Executor(hypro::HybridAutomaton<Number>& automaton, LocPtr initialLocation, Point initialValuation) :mAutomaton(automaton), mLastLocation(initialLocation), mLastState(initialValuation) {}
+using Matrix = hypro::matrix_t<Number>;
+using Vector = hypro::vector_t<Number>;
 
-        Point execute(const Point& ctrlInput);
-        hypro::HybridAutomaton<Number> &mAutomaton;
-        LocPtr mLastLocation;
-        Point mLastState;
-        hypro::Settings mSettings;
-    private:
+struct Executor {
+  Executor( hypro::HybridAutomaton<Number>& automaton, LocPtr initialLocation, Point initialValuation )
+      : mAutomaton( automaton ), mLastLocation( initialLocation ), mLastState( initialValuation ) {}
+
+  Point                           execute( const Point& ctrlInput );
+  hypro::HybridAutomaton<Number>& mAutomaton;
+  LocPtr                          mLastLocation;
+  Point mLastState;
+  hypro::Settings                 mSettings;
+  ExecutionSettings               mExecutionSettings;
+
+ private:
         double mCycleTime = 1.0;
         std::vector<hypro::ReachTreeNode<Representation>> roots;
         std::mt19937 mGenerator;
