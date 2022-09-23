f58189632d9f876a024666de64dc1dcce339a19f
Stefan Schupp <stefan.schupp@tuwien.ac.at>
2022-09-23 15:13:42 +0200

    add more tracks

--- racetracks/simpleL/waypoints.h
+++ racetracks/simpleL/waypoints.h
@@ -9,7 +9,6 @@ std::vector<hypro::Point<Number>> createWaypoints() {
 	auto res = std::vector<hypro::Point<Number>>{
 	hypro::Point<Number>{ 61.35735 , 17.2682 },
 	hypro::Point<Number>{ 17.89425 , 25.29215 },
-//      hypro::Point<Number>{ 40 , 25.29215 },
 	hypro::Point<Number>{ 21.196550000000002 , 42.075450000000004 },
 	hypro::Point<Number>{ 53.671350000000004 , 35.9296 },
 	hypro::Point<Number>{ 63.343149999999994 , 75.08985 },
