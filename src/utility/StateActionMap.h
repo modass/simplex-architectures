f9b5dc1bcd2f5d1121d37cbae40ce517ca406ad4
Stefan Schupp <stefan.schupp@tuwien.ac.at>
2023-07-04 10:53:44 +0200

    add first class and method stubs

---
+++ src/utility/StateActionMap.h
@@ -0,0 +1,47 @@
+/*
+ * Created by Stefan Schupp <stefan.schupp@tuwien.ac.at> on 04.07.23.
+ */
+
+#ifndef SIMPLEXARCHITECTURES_STATEACTIONMAP_H
+#define SIMPLEXARCHITECTURES_STATEACTIONMAP_H
+
+#include "../types.h"
+
+namespace simplexArchitectures {
+
+    template<typename ValueSet, typename Action>
+    struct ValueActionPair {
+        ValueSet values;
+        Action action;
+    };
+
+    template<typename ValueSet, typename Action>
+    class StateActionMap {
+        using VAPair = ValueActionPair<ValueSet, Action>;
+    public:
+
+        /**
+         * Loopup of the correct action for a given location and valuation. If not contained, returns an empty optional. If multiple actions are stored, returns the oldest one that has been stored.
+         * @param location The location
+         * @param valuation The valuation, represented as a point
+         * @return An optional either containing an action in case of success or nothing in case no action has been stored for the location-valuation pair
+         */
+        std::optional<Action> getAction(const Location &location, const Point &valuation) const;
+
+        /**
+         * Add information to the mapping
+         * @param location The location
+         * @param values The set of valuation
+         * @param action The targeted action for this state set
+         */
+        void add(const Location &location, const ValueSet &values, const Action &action);
+
+    protected:
+        std::map<Location *, std::vector<VAPair>> mValuations; ///< stores all information
+    };
+
+} // namespace
+
+#include "StateActionMap.tpp"
+
+#endif //SIMPLEXARCHITECTURES_STATEACTIONMAP_H
---
+++ src/utility/StateActionMap.tpp
@@ -0,0 +1,27 @@
+/*
+ * Created by Stefan Schupp <stefan.schupp@tuwien.ac.at> on 04.07.23.
+ */
+
+#include "StateActionMap.h"
+
+namespace simplexArchitectures {
+
+    template<typename ValueSet, typename Action>
+    std::optional<Action>
+    StateActionMap<ValueSet, Action>::getAction(const Location &location, const Point &valuation) const {
+        if (mValuations.find(&location) != mValuations.end()) {
+            for (const auto &[valuations, action]: mValuations[&location]) {
+                if (valuations.contains(valuation)) {
+                    return action;
+                }
+            }
+        }
+        return std::nullopt;
+    }
+
+    template<typename ValueSet, typename Action>
+    void StateActionMap<ValueSet, Action>::add(const Location &location, const ValueSet &values, const Action &action) {
+
+    }
+
+} // namespace
